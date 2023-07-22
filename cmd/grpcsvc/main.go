14853b2a4b17f5f4aec9ceeac287be42413fc707
dhij <davidhwang.ij@gmail.com>
2023-07-22 15:08:22 -0400

    add a comment to db source

--- cmd/grpcsvc/main.go
+++ cmd/grpcsvc/main.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	var (
 		dbDriver = "mysql"
+		// if you are running the mysql container locally without docker-compose, use dbSource = "root:password@tcp(localhost:33060)/notifier_db"
 		dbSource = "root:password@tcp(mysql:3306)/notifier_db"
 	)
 
